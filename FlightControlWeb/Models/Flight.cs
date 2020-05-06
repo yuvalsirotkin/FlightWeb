d1204910b0e083add5efb4c003c94e1c77e304a4
gal95elkayam <59448091+gal95elkayam@users.noreply.github.com>
2020-05-06 14:21:22 +0300

    Update Flight.cs

--- FlightControlWeb/Models/Flight.cs
+++ FlightControlWeb/Models/Flight.cs
@@ -7,18 +7,12 @@ namespace FlightControlWeb.Models
 {
     public class Flight
     {
-        public long Id { get; set; }
-        public string Name { get; set; }
-        public bool IsComplete { get; set; }
-        /*
-        public int Id { get; set; }
-        public double Longitude { get; set; }
-        public double Latitude { get; set; }
-        public int Passengers { get; set; }
-        public string Company_name { get; set; }
-        public string Date_time { get; set; }
-        public bool Is_external { get; set; }
-        */
-        //small test git sarah
+       public int flight_id { get; set; }
+        public double longitude { get; set; }
+        public double latitude { get; set; }
+        public int passengers { get; set; }
+        public string company_name { get; set; }
+        public string date_time { get; set; }
+        public bool is_external { get; set; }
     }
 }
